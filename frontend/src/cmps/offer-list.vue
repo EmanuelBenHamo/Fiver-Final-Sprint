7cad3070d1f059c952582e18e5d8534b1f048d87
Rotem <tetch6@gmail.com>
2020-03-20 13:53:26 +0200

    backoffice - offers

---
+++ frontend/src/cmps/offer-list.vue
@@ -0,0 +1,28 @@
+<template>
+<section class="offer-list-container">
+  <h1>Offer List</h1>
+    <offer-preview ></offer-preview>
+</section>
+</template>
+
+<script>
+import offerPreview from './offer-preview.vue';
+export default {
+    name: 'offer-list',
+    created(){
+        this.getOffers()
+    },
+    methods:{
+        // getOffers(){
+
+        // }
+    },
+    components: {
+        offerPreview
+    }
+}
+</script>
+
+<style>
+
+</style>
