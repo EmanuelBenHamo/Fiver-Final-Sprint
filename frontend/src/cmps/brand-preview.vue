f9b10f537b19b31dd557e12a8114335352455533
vova <vleygel@gmail.com>
2020-03-21 12:46:32 +0200

    merged master into branch

--- frontend/src/cmps/brand-preview.vue
+++ frontend/src/cmps/brand-preview.vue
@@ -1,8 +1,8 @@
 <template>
   <section class="brand-preview">
     <h1 class="brand-name">{{brand.name}}</h1>
-    <p class="brand-type">{{brand.customersCount}}</p>
-    <p class="in-stock">{{brand.marketValue}}</p>
+    <p class="brand-type"><span>Customers:</span>{{brand.customersCount}}</p>
+    <p class="in-stock"><span><Customers></Customers>:</span>{{brand.marketValue}}</p>
     <div class="preview-btns">
       <router-link :to="'/brandDetails/' + brand._id">
         <button class="brand-details-btn">Details</button>
