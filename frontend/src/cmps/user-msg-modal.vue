5fb1cc9fd1f930c49151c9f4a55ffbc2120a9088
vova <vleygel@gmail.com>
2020-03-23 20:39:47 +0200

    added an event bus and replaced the  alert with a modal

---
+++ frontend/src/cmps/user-msg-modal.vue
@@ -0,0 +1,32 @@
+<template>
+  <section>
+      <div v-if="isShown" class="msg-container"></div>
+    <transition name="fade">
+      <h1>hello maafaka{{msg.txt}}</h1>
+    </transition>
+  </section>
+</template>
+
+<script>
+import { eventBus } from "../services/event.bus.js";
+export default {
+  data() {
+    return {
+      isShown: false,
+      msg: {}
+    };
+  },
+  created() {
+    eventBus.$on("showMsg", msg => {
+      this.msg = msg;
+      this.isShown = true;
+      setTimeout(() => {
+        this.isShown = false;
+      }, 2000);
+    });
+  }
+};
+</script>
+
+<style>
+</style>
