d6c5dd2612af718e9c1eb5c6357cab40b12293ad
Rotem <tetch6@gmail.com>
2020-03-24 17:30:43 +0200

    influencer-filter-posts-still-not-working

---
+++ frontend/src/cmps/influencer-filter.vue
@@ -0,0 +1,101 @@
+<template>
+<section class="filter-container">
+<h3>Who is your next influencer?</h3>
+  <input type="text" v-model="filterBy.name" placeholder="Search By Name">
+  
+  <div class="select-btns gender-radio-btn flex">
+    <label class="radio-container radio-All">
+        <input type="radio" value="All" v-model="filterBy.gender">
+        <span class="checkmark">All</span>
+    </label>
+    <label class="radio-container radio-male">
+        <input type="radio" value="Male" v-model="filterBy.gender">
+        <span class="checkmark">Male</span>
+    </label>
+    <label class="radio-container radio-female">
+        <input type="radio" value="Female" v-model="filterBy.gender">
+        <span class="checkmark">Female</span>
+    </label>
+  </div>
+  <!-- Multy tags selector -->
+  <input type="number" v-model="filterBy.pricePerPost" placeholder="What is your max price?">
+
+
+    <div class="select-btns social-checkbox-btn flex">
+       <label class="checkbox-container checkbox-instagram">
+            <input type="checkbox" value="instagram" v-model="filterBy.socials.type">
+            <span class="checkmark">Instagram</span>
+        </label>
+         <label class="checkbox-container checkbox-snapchat">
+            <input type="checkbox" value="snapchat" v-model="filterBy.socials.type">
+            <span class="checkmark">Snapchat</span>
+        </label>
+        <label class="checkbox-container checkbox-tiktok">
+            <input type="checkbox" value="tiktok" v-model="filterBy.socials.type ">
+            <span class="checkmark">Tiktok</span>
+        </label>
+    </div>
+    <div class="followersCount">
+        <label>Folloewrs </label>
+        <span>- choose a range</span>
+        <el-slider
+            v-model="filterBy.socials.followersCount"
+            :format-tooltip="localeString"
+            range
+            label='Followers'
+            :steps="100"
+            :max="1000000">
+        </el-slider>
+    </div>
+    <div class="menFollowersPercentage flex space-between align-center">
+        <label>Men</label>
+        <div class="menFollowersPercentageSilder">
+            <el-slider 
+                v-model="filterBy.socials.menFollowersPercentage"
+                :format-tooltip="showMenPercentage">
+            </el-slider>
+        </div>
+        <label>Women</label>
+    </div>
+    <input type="number" v-model="filterBy.socials.posts" placeholder="Minimum Number of Posts">
+    <input type="number" v-model="filterBy.socials.stories" placeholder="Minimum Number of Stories">
+    <input type="number" v-model="filterBy.socials.followersAvgAge" placeholder="Followers Avrage Age">
+    <button class="btn" @click="setFilterBy">Filter</button>
+
+
+
+
+<!-- <pre>{{filterBy}}</pre> -->
+</section>
+</template>
+
+<script>
+export default {
+data(){
+    return{
+        filterBy: {
+            gender: 'All',
+            socials: {
+                type:[]
+            }
+        }
+    }
+},
+methods: {
+    localeString(value){
+        if(!value)return
+        return value.toLocaleString('en-US')
+    },
+    showMenPercentage(value) {
+        return value + '%   '
+    },
+    setFilterBy() {
+        this.$emit('setFilterBy', this.filterBy)
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
--- frontend/src/services/influencer.service.js
+++ frontend/src/services/influencer.service.js
@@ -4,76 +4,152 @@ const KEY = 'influencers';
 
 var gInfluencers = _getInfluencersFromStorage();
 
-
-function _getInfluencersFromStorage(){
-    var influencers = storageService.load(KEY);
-    if(!influencers){
-        influencers = require('../../data/influencers.json');
-        storageService.store(KEY, influencers)
-    }
-    return influencers
+function _getInfluencersFromStorage() {
+  var influencers = storageService.load(KEY);
+  if (!influencers) {
+    influencers = require('../../data/influencers.json');
+    storageService.store(KEY, influencers);
+  }
+  return influencers;
 }
-function query(filterBy = {}) {
-    return Promise.resolve(gInfluencers);
+async function query(filterBy) {
+  if (!filterBy) return await gInfluencers;
+  const influencersToShow = _filterInfluencers(filterBy);
+  return await influencersToShow;
 }
 
 async function getById(id) {
-    return await gInfluencers.find(influencer => influencer._id === id);
+  return await gInfluencers.find(influencer => influencer._id === id);
 }
 
 function remove(id) {
-    const idx = gInfluencers.findIndex(influencer => influencer._id === id);
+  const idx = gInfluencers.findIndex(influencer => influencer._id === id);
 
-    gInfluencers.splice(idx, 1);
-    storageService.store(KEY, gInfluencers)
+  gInfluencers.splice(idx, 1);
+  storageService.store(KEY, gInfluencers);
 
-    return Promise.resolve();
+  return Promise.resolve();
 }
 
 async function add(influencer) {
-    influencer._id = _randomInt(10000, 10000000) + ''
-    influencer.createdAt = Date.now();
-    influencer = _setSocialInfo(influencer)
-    gInfluencers.unshift(influencer);
-    storageService.store(KEY, gInfluencers)
-    
-    return influencer;
+  influencer._id = _randomInt(10000, 10000000) + '';
+  influencer.createdAt = Date.now();
+  influencer = _setSocialInfo(influencer);
+  gInfluencers.unshift(influencer);
+  storageService.store(KEY, gInfluencers);
+
+  return influencer;
 }
 
 function update(influencer) {
-    const idx = gInfluencers.findIndex(currInfluencer => currInfluencer._id === influencer._id);
+  const idx = gInfluencers.findIndex(
+    currInfluencer => currInfluencer._id === influencer._id
+  );
 
-    influencer.updatedAt = Date.now();
-    gInfluencers.splice(idx, 1, influencer);
-    storageService.store(KEY, gInfluencers)
+  influencer.updatedAt = Date.now();
+  gInfluencers.splice(idx, 1, influencer);
+  storageService.store(KEY, gInfluencers);
 
-    return Promise.resolve(influencer);
+  return Promise.resolve(influencer);
 }
 
 export default {
-    query,
-    getById,
-    remove,
-    add,
-    update
+  query,
+  getById,
+  remove,
+  add,
+  update
+};
+
+function _setSocialInfo(influencer) {
+  influencer.socials = influencer.socials.map(social => {
+    console.log('social', social);
+    var currSocial = {
+      type: social,
+      menFollowers: _randomInt(10000, 10000000),
+      womenFollowers: _randomInt(10000, 10000000),
+      posts: _randomInt(1000, 1000000),
+      stories: _randomInt(1000, 1000000),
+      avgAge: _randomInt(16, 50)
+    };
+    return currSocial;
+  });
+  return influencer;
 }
 
-function _setSocialInfo(influencer){
-    influencer.socials = influencer.socials.map(social => {
-        console.log('social', social);
-             var currSocial = {
-                type : social,
-                menFollowers : _randomInt(10000, 10000000),
-                womenFollowers : _randomInt(10000, 10000000),
-                posts :_randomInt(1000, 1000000),
-                stories : _randomInt(1000, 1000000),
-                avgAge: _randomInt(16, 50),
-            }
-            return currSocial
+function _filterInfluencers(filterBy) {
+  var name = new RegExp(filterBy.name, 'i');
+
+  const influencersToShow = gInfluencers
+    .filter(influencer => {
+      // Filter by name
+      return (
+        influencer.firstName.match(name) || influencer.lastName.match(name)
+      );
+    })
+    .filter(influencer => {
+      if (filterBy.gender === 'All') return influencer; //Filter by gender
+      return filterBy.gender === influencer.gender;
+    })
+    .filter(influencer => {
+      if (!filterBy.pricePerPost) return influencer; //Filter by price per post
+      return +filterBy.pricePerPost > +influencer.pricePerPost;
+    })
+    .filter(influencer => {
+      if (!filterBy.socials.type || !filterBy.socials.type.length){
+          influencer.filteredSocialMap = influencer.socials
+          return influencer
+      }
+      const filteredSocialMap = influencer.socials.filter(social => {
+        var socialType = filterBy.socials.type.filter(filterSocialType => { 
+          // filterBy.socials.type is an Array of types
+          return social.type === filterSocialType;
+        });
+        return socialType.length;
+      });
+      if (filteredSocialMap.length) {
+        influencer.filteredSocialMap = filteredSocialMap;
+        return influencer;
+      }
+    })
+    .filter(influencer => {
+      return influencer.filteredSocialMap.some(social => {
+        // is true if at least one of the social media stats is true
+        let followersCount = social.menFollowers + social.womenFollowers;
+        let menFollowersPercentage =
+          (social.menFollowers / followersCount) * 100;
+
+        return (
+          +followersCount > filterBy.socials.followersCount[0] &&
+          +followersCount < filterBy.socials.followersCount[1] &&
+          menFollowersPercentage > filterBy.socials.menFollowersPercentage
+        );
+      });
+    })
+    .filter(influencer => {
+        if(!filterBy.socials.posts) return influencer;
+        influencer.filteredSocialMap.some( social => {
+            console.log('SOCIAL IN POSTS FILTER', social);
+            
+            return +social.posts > +filterBy.socials.posts;
         })
-    return influencer
+    })
+    .filter(influencer => {
+        if(!filterBy.socials.stories) return influencer;
+        influencer.filteredSocialMap.some( social => {
+            return +social.stories > +filterBy.socials.stories;
+        })
+    })
+    .filter(influencer => {
+        if(!filterBy.socials.followersAvgAge) return influencer;
+        influencer.filteredSocialMap.some( social => {
+            return +social.followersAvgAge > +filterBy.socials.followersAvgAge;
+        })
+    })
+    
+  return influencersToShow;
 }
 
-function _randomInt(min , max){
-    return Math.floor(Math.random() * (max - min + 1) + min)
+function _randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
