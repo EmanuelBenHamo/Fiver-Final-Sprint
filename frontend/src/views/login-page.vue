18a7d84b41ee99c2d832899dff8866b31c47dbb1
vova <vleygel@gmail.com>
2020-03-29 13:45:55 +0300

    changes to login page

--- frontend/src/views/login-page.vue
+++ frontend/src/views/login-page.vue
@@ -1,18 +1,27 @@
 <template>
   <section>
-    <button class="btn" @click="credentials.userType='influencer'">I am an influencer</button>
-    <button class="btn" @click="credentials.userType='brand'">I am a brand</button>
-    <span v-if="credentials.userType">You are about to login as {{credentials.userType}}</span>
-    <form v-if="credentials.userType" @submit.prevent="login">
-      <label>
-        username:
-        <input type="text" placeholder="username" v-model="credentials.username" />
-      </label>
-      <label>
-        password:
-        <input type="password" placeholder="password" v-model="credentials.password" />
-      </label>
-      <button class="btn" type="submit">Sign In</button>
+    <form
+      class="login-form-container flex column"
+      v-if="credentials.userType"
+      @submit.prevent="login"
+    >
+      <div class="login-type-btns">
+        <button class="btn" @click="credentials.userType='influencer'">I am an influencer</button>
+        <button class="btn" @click="credentials.userType='brand'">I am a brand</button>
+      </div>
+      <span
+        class="login-type-txt"
+        v-if="credentials.userType"
+      >You are about to login as {{credentials.userType}}</span>
+      <div class="login-input">
+        <input type="password" placeholder="password" />
+      </div>
+      <div class="login-input">
+        <input type="text" placeholder="username" />
+      </div>
+      <div class="login-btn">
+        <button class="btn login-btn" type="submit">Sign In</button>
+      </div>
     </form>
   </section>
 </template>
